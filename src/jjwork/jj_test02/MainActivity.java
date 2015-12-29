867eebdee894693411429187c4f4d10570f06c16
JokerGeek <980461483@qq.com>
2015-12-29 17:41:55 +0800

    添加几个测试界面

--- src/jjwork/jj_test02/MainActivity.java
+++ src/jjwork/jj_test02/MainActivity.java
@@ -1,9 +1,13 @@
 package jjwork.jj_test02;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +15,40 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button btn1 = (Button)findViewById(R.id.button1);
+        Button btn2 = (Button)findViewById(R.id.button2);
+        Button btn3 = (Button)findViewById(R.id.button3);
+        Button btn4 = (Button)findViewById(R.id.button4);
+        
+        btn1.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this, Sub1Activity.class));
+			}
+		});
+        btn2.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this, Sub2Activity.class));
+			}
+		});
+        btn3.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this, Sub3Activity.class));
+			}
+		});
+        btn4.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this, Sub4Activity.class));
+			}
+		});
     }
+    
 }
