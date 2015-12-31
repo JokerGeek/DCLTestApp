017957d6a6379169de57c454310520268d188332
jj <home.hong@qq.com>
2015-12-31 17:26:00 +0800

    settingActivity使用新的API PreferenceFragment

--- gen/jjwork/jj_test02/R.java
+++ gen/jjwork/jj_test02/R.java
@@ -8,17 +8,31 @@
 package jjwork.jj_test02;
 
 public final class R {
+    public static final class array {
+        public static final int comm_baud_strings=0x7f050001;
+        public static final int comm_baud_values=0x7f050000;
+        public static final int record_max_number_strings=0x7f050004;
+        public static final int record_max_number_values=0x7f050005;
+        public static final int test_time_strings=0x7f050002;
+        public static final int test_time_values=0x7f050003;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int yellow=0x7f080000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060001;
-        public static final int button2=0x7f060002;
-        public static final int button3=0x7f060003;
-        public static final int button4=0x7f060004;
-        public static final int textView1=0x7f060000;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int button3=0x7f090003;
+        public static final int button4=0x7f090004;
+        public static final int button5=0x7f090006;
+        public static final int button6=0x7f090007;
+        public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -28,8 +42,7 @@ public final class R {
         public static final int activity_sub4=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_name=0x7f060000;
     }
     public static final class style {
         /** 
@@ -53,10 +66,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
